328de3d487c38b20a58acdb993732659e7001d03
chamano sidney <chamano.sr@gmail.com>
2020-12-01 08:24:53 +0200

    setting API

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
-  
+
+  const APP_ID = "bc5b9997";
+  const APP_KEY = "6b9ce657f7ddb1bfb45c22296711d712";
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+
   return (
     <div className="App">
       <h1>Hello React</h1>
