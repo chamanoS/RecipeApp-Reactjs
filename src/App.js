1cf40f64f04a36007b2bb2dc24989924c6ed1a5a
chamano sidney <chamano.sr@gmail.com>
2020-12-01 09:04:34 +0200

    passing data from comp

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Recipe from './Recipe';
 import './App.css';
 
 const App = () => {
@@ -16,6 +17,7 @@ const App = () => {
     const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
     const data = await response.json()
     setRecipes(data.hits)
+    console.log(data.hits)
   }
   
   return (
@@ -24,6 +26,11 @@ const App = () => {
         <input className="search-bar" type="text"/>
         <button className="search-button" type="submit">Search</button>
       </form>
+      {recipes.map(recipe =>(
+        <Recipe title={recipe.recipe.label} 
+         calories={recipe.recipe.calories}
+         image={recipe.recipe.image}/>
+      ))}
     </div>
   );
 }
